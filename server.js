edf816cde61dd3e98c497e0057fcd3a68ff261da
Gagan Jadaun <gagan2020jadon@gmail.com>
2026-05-02 11:50:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const twilio = require('twilio');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // Initialize Twilio Client
 let twilioClient;
